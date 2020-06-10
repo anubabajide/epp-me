8f0b3f5e29a1e28334256cfe5ce073021f10495d
Aanuoluwapo-Babajide <anubabajide@gmail.com>
2020-06-10 05:04:14 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { options, proxyuri, key } from './utils/utils';
 
-function App() {
+const App:React.FC = () => {
+
+  const [radius, setRadius] = useState<number>(5000);
+  const [latitude, setLatitude] = useState<number | undefined>();
+  const [longitude, setLongitude] = useState<number | undefined>();
+  const [results, setResults] = useState<any[] | undefined>();
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition( position => {
+        let latitude = position.coords.latitude
+        setLatitude(latitude)
+        let longitude = position.coords.longitude
+        setLongitude(longitude)
+      });
+    }
+    else {
+      alert("Enable location")
+    }
+  },[])
+
+  const getResults = () => {
+    const url:string = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&type=hospital&radius=${radius}&key=${key}`;
+    fetch(proxyuri + url)
+      .then(res => res.json())
+      .then(data => setResults(data.results))
+      .catch(error => alert(error))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <div className="header__text">
+          <h1 className="header__primary">
+            Hospital Locator
+          </h1>
+          <span className="header__secondary">Locate Nearby Hospitals Close to Your Location</span>
+          <a className="header__btn" href="#search">Give it a try</a>
+        </div>
       </header>
-    </div>
+      <main className="main" id="search">
+        <div className="row margin--b">
+          <h2 className="main__row--text">Select Search Radius</h2>
+          <select 
+            className="main__row--radius"
+            onChange = {(e)=> setRadius(+e.target.value)}
+          >
+            {options.map(option => (
+              <option key={option.label} 
+              value={option.value}>{option.label}</option>
+            ))}
+          </select>
+          <button 
+            className="main__btn--search"
+            onClick={getResults}
+          >
+            Search
+          </button>
+        </div>
+        <div className="row">
+          {results && results.map(result => (
+            <div 
+              className="row display"
+              key={result.id}
+            >
+              <span className="main__results">Hospital Name: {result.name}</span>
+              <span className="main__results">Operational Status: {result.business_status}</span>
+              <span className="main__results">Hospital Address: {result.vicinity}</span>
+            </div>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
 
---
+++ src/utils/utils.ts
@@ -0,0 +1,10 @@
+export const options = [
+    { label: "5 km", value: 5000,},
+    { label: "10 km", value: 10000 },
+    { label: "15 km", value: 15000 },
+    { label: "20 km", value: 20000 
+}];
+  
+export const proxyuri = "https://cors-anywhere.herokuapp.com/";
+
+export const key = 'AIzaSyBbhLmqbMdEnny8r_NgSq57yVUQRWpUUTI';
